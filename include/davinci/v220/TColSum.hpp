5d6ee2caaf68608b0b4fcc7198a47139e208ce4c
yangweiliang <yangweiliang3@huawei.com>
2025-11-07 14:48:40 +0800

    TColSum

---
+++ include/davinci/v220/TColSum.hpp
@@ -0,0 +1,139 @@
+#ifndef TCOLSUM_HPP
+#define TCOLSUM_HPP
+
+#include "common/constants.hpp"
+#include "utils.hpp"
+
+namespace pto {
+    template <typename T, typename TileDataDst, typename TileDataSrc, typename TileDataTmp,
+              unsigned dststride, unsigned srcstride>
+    __tf__ __aicore__ PTO_INLINE void TColSum(typename TileDataDst::TileDType __out__ dst,
+                                              typename TileDataSrc::TileDType __in__ src,
+                                              typename TileDataTmp::TileDType __in__ tmp,
+                                              unsigned validRow, unsigned validCol, bool IsBinary) {
+        __ubuf__ T *dstPtr = (__ubuf__ T *)__cce_get_tile_ptr(dst);
+        __ubuf__ T *srcPtr = (__ubuf__ T *)__cce_get_tile_ptr(src);
+        __ubuf__ T *tmpPtr = (__ubuf__ T *)__cce_get_tile_ptr(tmp);
+
+        constexpr unsigned DTypeSize = sizeof(T);
+        constexpr unsigned blockSizeElem = BLOCK_BYTE_SIZE / DTypeSize;
+        constexpr unsigned elementsPerRepeat = REPEAT_BYTE / DTypeSize;
+        unsigned numRepeatPerLine = validCol / elementsPerRepeat;
+        unsigned numRemainPerLine = validCol % elementsPerRepeat;
+        unsigned numBlockPerLine = (srcstride * DTypeSize + BLOCK_BYTE_SIZE - 1) / BLOCK_BYTE_SIZE;
+        unsigned dupSrcStride = numBlockPerLine * blockSizeElem;
+        unsigned elementsPerLine = numRepeatPerLine * elementsPerRepeat;
+        unsigned lenBurst = (validCol * DTypeSize + BLOCK_BYTE_SIZE - 1) / BLOCK_BYTE_SIZE;
+
+        if (IsBinary) {
+            if (numRepeatPerLine > 0) {
+                set_mask_count();
+                set_vector_mask(0, elementsPerLine);
+                for (int i = 0; i < validRow / 2; i++) {
+                    vadd(tmpPtr + i * dupSrcStride, srcPtr + 2 * i * dupSrcStride,
+                         srcPtr + (2 * i + 1) * dupSrcStride, 0, 1, 1, 1, 8, 8, 8);
+                    pipe_barrier(PIPE_V);
+                }
+
+                if (validRow % 2 == 1) {
+                    vadd(tmpPtr, tmpPtr, srcPtr + (validRow - 1) * dupSrcStride,
+                         0, 1, 1, 1, 8, 8, 8);
+                    pipe_barrier(PIPE_V);
+                }
+                set_mask_norm();
+                set_vector_mask(-1, -1);
+            }
+
+            if (numRemainPerLine) {
+                SetContinuousMask(numRemainPerLine);
+                for (int i = 0; i < validRow / 2; i++) {
+                    vadd(tmpPtr + elementsPerLine + i * dupSrcStride, srcPtr + elementsPerLine + 2 * i * dupSrcStride,
+                         srcPtr + elementsPerLine + (2 * i + 1) * dupSrcStride, 1, 1, 1, 1, 8, 8, 8);
+                    pipe_barrier(PIPE_V);
+                }
+
+                if (validRow % 2 == 1) {
+                    vadd(tmpPtr + elementsPerLine, tmpPtr + elementsPerLine,
+                         srcPtr + elementsPerLine + (validRow - 1) * dupSrcStride, 1, 1, 1, 1, 8, 8, 8);
+                    pipe_barrier(PIPE_V);
+                }
+                set_vector_mask(-1, -1);
+            }
+
+            unsigned cnt = validRow / 2;
+            while (cnt > 1)
+            {
+                if (numRepeatPerLine > 0) {
+                    set_mask_count();
+                    set_vector_mask(0, elementsPerLine);
+                    for (int i = 0; i < cnt / 2; i++) {
+                        vadd(tmpPtr + i * dupSrcStride, tmpPtr + 2 * i * dupSrcStride,
+                             tmpPtr + (2 * i + 1) * dupSrcStride, 0, 1, 1, 1, 8, 8, 8);
+                        pipe_barrier(PIPE_V);
+                    }
+
+                    set_mask_norm();
+                    set_vector_mask(-1, -1);
+                }
+                if (numRemainPerLine) {
+                    SetContinuousMask(numRemainPerLine);
+                    for (int i = 0; i < cnt / 2; i++) {
+                        vadd(tmpPtr + elementsPerLine + i * dupSrcStride, tmpPtr + elementsPerLine + 2 * i * dupSrcStride,
+                             tmpPtr + elementsPerLine + (2 * i + 1) * dupSrcStride, 1, 1, 1, 1, 8, 8, 8);
+                        pipe_barrier(PIPE_V);
+                    }
+                    set_vector_mask(-1, -1);
+                }
+                cnt /= 2;
+            }
+            copy_ubuf_to_ubuf(dstPtr, tmpPtr, 0, 1, lenBurst, 0, 0);
+            pipe_barrier(PIPE_V);
+        } else {
+            copy_ubuf_to_ubuf(dstPtr, tmpPtr, 0, 1, lenBurst, 0, 0);
+            pipe_barrier(PIPE_V);
+
+            if (numRepeatPerLine > 0) {
+                set_mask_count();
+                set_vector_mask(0, elementsPerLine);
+                for (int i = 1; i < validRow; i++) {
+                    vadd(dstPtr, dstPtr, srcPtr + i * dupSrcStride, 0, 1, 1, 1, 8, 8, 8);
+                    pipe_barrier(PIPE_V);
+                }
+                set_mask_norm();
+                set_vector_mask(-1, -1);
+            }
+
+            dstPtr += elementsPerLine;
+            srcPtr += elementsPerLine;
+
+            if (numRemainPerLine) {
+                SetContinuousMask(numRemainPerLine);
+                for (int i = 0; i < validRow; i++) {
+                    vadd(dstPtr, dstPtr, srcPtr + i * dupSrcStride, 1, 1, 1, 1, 8, 8, 8);
+                    pipe_barrier(PIPE_V);
+                }
+                set_vector_mask(-1, -1);
+            }
+        }
+    }
+
+    template <typename TileDataDst, typename TileDataSrc, typename TileDataTmp>
+    __aicore__ PTO_INLINE void TCOLSUM(TileDataDst &dst, TileDataSrc &src, TileDataTmp &tmp, bool IsBinary) {
+        static_assert((sizeof(typename TileDataSrc::DType) == 2) || (sizeof(typename TileDataSrc::DType) == 4),
+                      "Data type must be 16/32");
+        static_assert(TileDataSrc::Loc == pto::Loaction::Vec, "Src location must be Vec!");
+        static_assert((TileDataSrc::layout == pto::Layout::ND) &&
+                      (TileDataSrc::isRowMajor && (TileDataSrc::SFractal == SLayout::NoneBox)) &&
+                      (TileDataDst::layout == pto::Layout::ND) &&
+                      (TileDataDst::isRowMajor && (TileDataDst::SFractal == SLayout::NoneBox)),
+                      "Src and dst layout must be ND!");
+        
+        constexpr unsigned dststride = TileDataDst::RowStride;
+        constexpr unsigned srcstride = TileDataSrc::RowStride;
+        unsigned validRow = src.GetValidRow();
+        unsigned validCol = src.GetValidCol();
+        TColSum<typename TileDataSrc::DType, TileDataDst, TileDataSrc, TileDataTmp, dststride,
+                srcstride>(dst.data(), src.data(), tmp.data(), validRow, validCol, IsBinary);
+    }
+}
+#endif
