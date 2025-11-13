55aeec79c0ea8d8ccaf71ff1326cf20da9e7cdd7
zhao_lulu <zhaojiamin13@huawei.com>
2025-11-13 16:09:32 +0800

    add TMATMUL ST case on A3

--- test/davinci/v220/src/st/testcase/tmatmul/tmatmul_kernel.cpp
+++ test/davinci/v220/src/st/testcase/tmatmul/tmatmul_kernel.cpp
@@ -28,6 +28,17 @@ __aicore__ inline unsigned CalcLinearOffset(unsigned GmShape1, unsigned Offset0,
 {
     return Offset1 + Offset0 * GmShape1;
 }
+
+template <typename T>
+__aicore__ inline void DynL1CopyInND(__cbuf__ T *dst, __gm__ T *src, unsigned TShape)
+{
+    uint16_t nBrust = 1;
+    uint16_t lenBrust = TShape * sizeof(T) / 32;
+    uint16_t srcStride = 0;
+    uint16_t dstStride = 0;
+    copy_gm_to_cbuf(dst, src, 0, nBrust, lenBrust, srcStride, dstStride, (pad_t)0/* padNode */);
+}
+
 template <typename GMT, typename L1T>
 __aicore__ inline void DynL1CopyIn(__cbuf__ L1T *dst, __gm__ GMT *src, unsigned TShape0, unsigned TShape1,
     unsigned GmShape0, unsigned GmShape1, unsigned GmOffset0, unsigned GmOffset1, int reserved)
@@ -263,6 +274,22 @@ __aicore__ inline void DynL1ToL0Bt(
     }
 }
 
+template <typename B>
+__aicore__ inline void DynL1ToBt(uint64_t dst, __cbuf__ B *src, unsigned len)
+{
+    constexpr uint16_t blockCubeK = BLOCK_ALIGN_BYTE / sizeof(B);
+    bool convControl = false;
+    if constexpr (std::is_same_v<B, half>){
+        convControl = true;
+    }
+    uint16_t nBrust = 1;
+    uint16_t blockLen = CeilAlign<uint16_t>(len, blockCubeK) / blockCubeK;
+    uint16_t sourceGap = 0;
+    uint16_t dstGap = 0;
+
+    copy_cbuf_to_bt(dst, src, convControl, nBrust, blockLen, sourceGap, dstGap);
+}
+
 template <typename T, typename U, typename S, int M, int K, int N>
 __aicore__ inline void runTMATMUL(__gm__ T *out, __gm__ U *src0, __gm__ S *src1)
 {
@@ -333,8 +360,8 @@ __aicore__ inline void runTMATMUL(__gm__ T *out, __gm__ U *src0, __gm__ S *src1)
 }   
 
 
-template <typename T, typename U, typename S, int M, int K, int N>
-__aicore__ inline void runTMATMUL_SPLIT_K(__gm__ T *out, __gm__ U *src0, __gm__ S *src1)
+template <typename T, typename U, typename S, typename B, int M, int K, int N, bool is_bias>
+__aicore__ inline void runTMATMUL_SPLIT_K(__gm__ T *out, __gm__ U *src0, __gm__ S *src1, __gm__ B *src2)
 {
     // static shape
     using GlobalDataSrc0 = GlobalTensor<U, Shape<1, 1, 1, M, K>, Stride<1 * M * K, 1 * M * K, M * K, K, 1>>;
@@ -348,43 +375,55 @@ __aicore__ inline void runTMATMUL_SPLIT_K(__gm__ T *out, __gm__ U *src0, __gm__ 
     GlobalDataSrc1 src1Global(src1);
     GlobalDataOut dstGlobal(out);
 
-    using TileMatAData =
-        Tile<Location::Mat, U, BASEM, K, BLayout::ColMajor, M, K, SLayout::RowMajor, 512>;  // L1上都是大n小z
+    using TileMatAData = Tile<Location::Mat, U, BASEM, K, BLayout::ColMajor, M, K, SLayout::RowMajor, 512>;  // L1上都是大n小z
     using TileMatBData = Tile<Location::Mat, S, K, BASEN, BLayout::ColMajor, K, N, SLayout::RowMajor, 512>;
+    using TileBiasData = Tile<Location::Mat, B, 1, BASEN, BLayout::RowMajor, 1, BASEN>;
 
     using LeftTile = TileLeft<U, BASEM, BASEK, M, BASEK>;
     using RightTile = TileRight<S, BASEK, BASEN, BASEK, N>;
     using AccTile = TileAcc<T, 128, BASEN, M, N>;
+    using BiasTile = Tile<Location::Bias, B, 1, BASEN, BLayout::RowMajor, 1, BASEN>;
 
     TileMatAData aMatTile;
     TileMatBData bMatTile;
+    TileBiasData biasDataTile;
 
     TASSIGN(aMatTile, 0x0);
     TASSIGN(bMatTile, 0x10000);
+    TASSIGN(biasDataTile, 0x20000);
 
     LeftTile aTile;
     RightTile bTile;
     AccTile cTile;
+    BiasTile biasTile;
+
     TASSIGN(aTile, 0x0);
     TASSIGN(bTile, 0x0);
     TASSIGN(cTile, 0x0);
+    TASSIGN(biasTile, 0x0);
 
     using AType = typename LeftTile::DType;
     using BType = typename RightTile::DType;
     using CType = typename AccTile::DType;
+    using BiasType = typename TileBiasData::DType;
 
     __cbuf__ AType *srcAAddr = aMatTile.data();
     __cbuf__ BType *srcBAddr = bMatTile.data();
+    __cbuf__ BiasType *srcBiasAddr = biasDataTile.data();
 
     __ca__ AType *a = (__ca__ AType *)(aTile.data());
     __cb__ BType *b = (__cb__ BType *)(bTile.data());
     __cc__ CType *c = (__cc__ CType *)(cTile.data());
+    uint64_t bias = biasTile.data();
 
     constexpr int iter = K / BASEK;
     for (int i = 0; i < iter; i++) { // baseK = 64
         /******************************TLOAD*****************************/
         DynL1CopyIn<U, U>(srcAAddr, src0, M, BASEK, M, K, 0, i * BASEK, 0);
         DynL1CopyIn<S, S>(srcBAddr, src1, BASEK, N, K, N, i * BASEK, 0, 0);
+        if constexpr(is_bias){
+            DynL1CopyInND<B>(srcBiasAddr, src2, BASEN);
+        }
 
         set_flag(PIPE_MTE2, PIPE_MTE1, EVENT_ID0);
         wait_flag(PIPE_MTE2, PIPE_MTE1, EVENT_ID0);
@@ -392,12 +431,19 @@ __aicore__ inline void runTMATMUL_SPLIT_K(__gm__ T *out, __gm__ U *src0, __gm__ 
         /**************************TMOV && TEXTRACT**************************/
         DynL1ToL0A<U, 0, 0>(a, srcAAddr, M, BASEK, M, BASEK);
         DynL1ToL0B<S, 0, 0>(b, srcBAddr, BASEK, N, BASEK, N); // Nz2Zn [K, N]
+        if constexpr(is_bias){
+            DynL1ToBt<B>(bias, srcBiasAddr, BASEN);
+        }
 
         set_flag(PIPE_MTE1, PIPE_M, EVENT_ID0);
         wait_flag(PIPE_MTE1, PIPE_M, EVENT_ID0);
 
         if (i == 0) {
-            TMATMUL(cTile, aTile, bTile);  // L0C清空
+            if constexpr(is_bias){
+                TMATMUL_BIAS(cTile, aTile, bTile, biasTile);
+            } else {
+                TMATMUL(cTile, aTile, bTile);  // L0C清空
+            }
         } else {
             TMATMUL_ACC(cTile, cTile, aTile, bTile);  // L0C不清空
         }
@@ -411,6 +457,80 @@ __aicore__ inline void runTMATMUL_SPLIT_K(__gm__ T *out, __gm__ U *src0, __gm__ 
 
     out = dstGlobal.data();
 }
+
+template <typename T, typename U, typename S, typename B, int M, int K, int N, int ValidM, int ValidK, int ValidN>
+__aicore__ inline void runTMATMUL_BIAS(__gm__ T *out, __gm__ U *src0, __gm__ S *src1, __gm__ B *src2)
+{
+    // static shape
+    using GlobalDataOut = GlobalTensor<T, Shape<1, 1, 1, M, N>, Stride<1 * M * N, 1 * M * N, M * N, N, 1>>;
+    GlobalDataOut dstGlobal(out);
+    
+    using TileMatAData = Tile<Location::Mat, U, M, K, BLayout::ColMajor, M, K, SLayout::RowMajor, 512>; // L1上都是大n小z
+    using TileMatBData = Tile<Location::Mat, S, K, N, BLayout::ColMajor, K, N, SLayout::RowMajor, 512>;
+    using TileBiasData = Tile<Location::Mat, B, 1, N, BLayout::RowMajor, 1, N>;
+
+    using LeftTile = TileLeft<U, M, K, M, K>;
+    using RightTile = TileRight<S, K, N, K, N>;
+    using AccTile = TileAcc<T, M, N, M, N>;
+    using BiasTile = Tile<Location::Bias, B, 1, N, BLayout::RowMajor, 1, N>;
+
+    TileMatAData aMatTile;
+    TileMatBData bMatTile;
+    TileBiasData biasMatTile;
+    TASSIGN(aMatTile, 0x0);
+    TASSIGN(bMatTile, 0x10000);
+    TASSIGN(biasMatTile, 0x20000);
+
+    LeftTile aTile;
+    RightTile bTile;
+    AccTile cTile;
+    BiasTile biasTile;
+    TASSIGN(aTile, 0x0);
+    TASSIGN(bTile, 0x0);
+    TASSIGN(cTile, 0x0);
+    TASSIGN(biasTile, 0x0);
+
+    using AType = typename LeftTile::DType;
+    using BType = typename RightTile::DType;
+    using CType = typename AccTile::DType;
+    using BiasType = typename BiasTile::DType;
+
+    __cbuf__ AType *srcAAddr = aMatTile.data();
+    __cbuf__ BType *srcBAddr = bMatTile.data();
+    __cbuf__ BiasType *srcBiasAddr = biasMatTile.data();
+
+    __ca__ AType *a = (__ca__ AType *)(aTile.data());
+    __cb__ BType *b = (__cb__ BType *)(bTile.data());
+    __cc__ CType *c = (__cc__ CType *)(cTile.data());
+    uint64_t bias = biasTile.data();
+
+    /******************************TLOAD*****************************/
+
+    DynL1CopyIn<U, U>(srcAAddr, src0, ValidM, ValidK, ValidM, ValidK, 0, 0, 0);
+    DynL1CopyIn<S, S>(srcBAddr, src1, ValidK, ValidN, ValidK, ValidN, 0, 0, 0);
+
+    DynL1CopyInND<B>(srcBiasAddr, src2, N);
+
+    set_flag(PIPE_MTE2, PIPE_MTE1, EVENT_ID0);
+    wait_flag(PIPE_MTE2, PIPE_MTE1, EVENT_ID0);
+
+    /**************************TMOV && TEXTRACT**************************/
+    DynL1ToL0A<U, 0, 0>(a, srcAAddr, ValidM, ValidK, ValidM, ValidK);
+    DynL1ToL0B<S, 0, 0>(b, srcBAddr, ValidK, ValidN, ValidK, ValidN); // Nz2Zn [K, N]
+    DynL1ToBt<B>(bias, srcBiasAddr, ValidN);
+
+    set_flag(PIPE_MTE1, PIPE_M, EVENT_ID0);
+    wait_flag(PIPE_MTE1, PIPE_M, EVENT_ID0);
+
+    TMATMUL_BIAS(cTile, aTile, bTile, biasTile);
+
+    set_flag(PIPE_M, PIPE_FIX, EVENT_ID0);
+    wait_flag(PIPE_M, PIPE_FIX, EVENT_ID0);
+
+    /********************************TSTORE****************************/
+    L0CCopyOut<T, T, M, N, M, N>(out, c, ValidM, ValidN, 0, 0, 0);
+    out = dstGlobal.data();
+}   
 
 extern "C" __global__ __aicore__ void launchTMATMUL_2(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1)
 {
@@ -440,9 +560,100 @@ extern "C" __global__ __aicore__ void launchTMATMUL_3(__gm__ uint8_t *out, __gm_
     constexpr uint32_t N = 63;
     constexpr uint32_t K = 128;
 
-    runTMATMUL_SPLIT_K<float, half, half, M, K, N>(reinterpret_cast<__gm__ float *>(out),
+    runTMATMUL_SPLIT_K<float, half, half, float, M, K, N, false>(reinterpret_cast<__gm__ float *>(out),
         reinterpret_cast<__gm__ half *>(src0),
-        reinterpret_cast<__gm__ half *>(src1));
+        reinterpret_cast<__gm__ half *>(src1),
+        nullptr);
+}
+
+extern "C" __global__ __aicore__ void launchTMATMULBIAS_1(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1, __gm__ uint8_t *src2)
+{
+    constexpr uint32_t M = 128;
+    constexpr uint32_t N = 64;
+    constexpr uint32_t K = 128;
+
+    runTMATMUL_BIAS<float, half, half, float, M, K, N, M, K, N>(reinterpret_cast<__gm__ float *>(out),
+        reinterpret_cast<__gm__ half *>(src0),
+        reinterpret_cast<__gm__ half *>(src1),
+        reinterpret_cast<__gm__ float *>(src2));
+}
+
+extern "C" __global__ __aicore__ void launchTMATMULBIAS_2(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1, __gm__ uint8_t *src2)
+{
+    constexpr uint32_t M = 128;
+    constexpr uint32_t N = 64;
+    constexpr uint32_t K = 128;
+    constexpr uint32_t ValidN = 63;
+
+    runTMATMUL_BIAS<float, half, half, float, M, K, N, M, K, ValidN>(reinterpret_cast<__gm__ float *>(out),
+        reinterpret_cast<__gm__ half *>(src0),
+        reinterpret_cast<__gm__ half *>(src1),
+        reinterpret_cast<__gm__ float *>(src2));
+}
+
+extern "C" __global__ __aicore__ void launchTMATMULBIAS_3(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1, __gm__ uint8_t *src2)
+{
+    constexpr uint32_t M = 16;
+    constexpr uint32_t N = 16;
+    constexpr uint32_t K = 16;
+    constexpr uint32_t ValidM = 15;
+    constexpr uint32_t ValidN = 15;
+
+    runTMATMUL_BIAS<float, float, float, float, M, K, N, ValidM, K, ValidN>(reinterpret_cast<__gm__ float *>(out),
+        reinterpret_cast<__gm__ float *>(src0),
+        reinterpret_cast<__gm__ float *>(src1),
+        reinterpret_cast<__gm__ float *>(src2));
+}
+
+extern "C" __global__ __aicore__ void launchTMATMULBIAS_4(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1, __gm__ uint8_t *src2)
+{
+    constexpr uint32_t M = 128;
+    constexpr uint32_t N = 64;
+    constexpr uint32_t K = 128;
+    constexpr uint32_t ValidM = 127;
+    constexpr uint32_t ValidN = 63;
+    constexpr uint32_t ValidK = 127;
+
+    runTMATMUL_BIAS<int32_t, int8_t, int8_t, int32_t, M, K, N, ValidM, ValidK, ValidN>(reinterpret_cast<__gm__ int32_t *>(out),
+        reinterpret_cast<__gm__ int8_t *>(src0),
+        reinterpret_cast<__gm__ int8_t *>(src1),
+        reinterpret_cast<__gm__ int32_t *>(src2));
+}
+
+extern "C" __global__ __aicore__ void launchTMATMULBIAS_5(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1, __gm__ uint8_t *src2)
+{
+    constexpr uint32_t M = 128;
+    constexpr uint32_t N = 64;
+    constexpr uint32_t K = 128;
+
+    runTMATMUL_BIAS<float, bfloat16_t, bfloat16_t, float, M, K, N, M, K, N>(reinterpret_cast<__gm__ float *>(out),
+        reinterpret_cast<__gm__ bfloat16_t *>(src0),
+        reinterpret_cast<__gm__ bfloat16_t *>(src1),
+        reinterpret_cast<__gm__ float *>(src2));
+}
+
+extern "C" __global__ __aicore__ void launchTMATMULBIAS_6(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1, __gm__ uint8_t *src2)
+{
+    constexpr uint32_t M = 128;
+    constexpr uint32_t N = 64;
+    constexpr uint32_t K = 128;
+
+    runTMATMUL_BIAS<int32_t, int8_t, int8_t, int32_t, M, K, N, M, K, N>(reinterpret_cast<__gm__ int32_t *>(out),
+        reinterpret_cast<__gm__ int8_t *>(src0),
+        reinterpret_cast<__gm__ int8_t *>(src1),
+        reinterpret_cast<__gm__ int32_t *>(src2));
+}
+
+extern "C" __global__ __aicore__ void launchTMATMULBIAS_7(__gm__ uint8_t *out, __gm__ uint8_t *src0, __gm__ uint8_t *src1, __gm__ uint8_t *src2)
+{
+    constexpr uint32_t M = 128;
+    constexpr uint32_t N = 64;
+    constexpr uint32_t K = 128;
+
+    runTMATMUL_SPLIT_K<int32_t, int8_t, int8_t, int32_t, M, K, N, true>(reinterpret_cast<__gm__ int32_t *>(out),
+        reinterpret_cast<__gm__ int8_t *>(src0),
+        reinterpret_cast<__gm__ int8_t *>(src1),
+        reinterpret_cast<__gm__ int32_t *>(src2));
 }
 
 template <int32_t tilingKey>
@@ -457,6 +668,34 @@ void launchTMATMUL(uint8_t *out, uint8_t *src0, uint8_t *src1, void *stream)
     }
 }
 
+template <int32_t tilingKey>
+void launchTMATMUL_BIAS(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream)
+{
+    if constexpr (tilingKey == 1) {
+        launchTMATMULBIAS_1<<<1, nullptr, stream>>>(out, src0, src1, src2);
+    } else if constexpr (tilingKey == 2) {
+        launchTMATMULBIAS_2<<<1, nullptr, stream>>>(out, src0, src1, src2);
+    } else if constexpr (tilingKey == 3) {
+        launchTMATMULBIAS_3<<<1, nullptr, stream>>>(out, src0, src1, src2);
+    } else if constexpr (tilingKey == 4) {
+        launchTMATMULBIAS_4<<<1, nullptr, stream>>>(out, src0, src1, src2);
+    } else if constexpr (tilingKey == 5) {
+        launchTMATMULBIAS_5<<<1, nullptr, stream>>>(out, src0, src1, src2);
+    } else if constexpr (tilingKey == 6) {
+        launchTMATMULBIAS_6<<<1, nullptr, stream>>>(out, src0, src1, src2);
+    } else if constexpr (tilingKey == 7) {
+        launchTMATMULBIAS_7<<<1, nullptr, stream>>>(out, src0, src1, src2);
+    }
+}
+
 template void launchTMATMUL<1>(uint8_t *out, uint8_t *src0, uint8_t *src1, void *stream);  // 实例化 tilingKey=1
 template void launchTMATMUL<2>(uint8_t *out, uint8_t *src0, uint8_t *src1, void *stream);  // 实例化 tilingKey=2
 template void launchTMATMUL<3>(uint8_t *out, uint8_t *src0, uint8_t *src1, void *stream);  // 实例化 tilingKey=3
+
+template void launchTMATMUL_BIAS<1>(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
+template void launchTMATMUL_BIAS<2>(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
+template void launchTMATMUL_BIAS<3>(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
+template void launchTMATMUL_BIAS<4>(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
+template void launchTMATMUL_BIAS<5>(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
+template void launchTMATMUL_BIAS<6>(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
+template void launchTMATMUL_BIAS<7>(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
