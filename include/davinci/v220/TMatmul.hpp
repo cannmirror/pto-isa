dbfe8fc8120e4e45b63dfc018b7dfa162236f226
zhao_lulu <zhaojiamin13@huawei.com>
2025-12-02 16:18:28 +0800

    add dynamic check in debug mode for TMATMUL in A3
    Co-authored-by: zhao_lulu<zhaojiamin13@huawei.com>

--- include/davinci/v220/TMatmul.hpp
+++ include/davinci/v220/TMatmul.hpp
@@ -2,95 +2,99 @@
 #define TMATMUL_HPP
 
 namespace pto {
-    template <typename TileAcc, typename TileLeft, typename TileRight, bool cmatrixSource, bool CmatrixInitVal>
-    __tf__ __aicore__ void TMatmul(typename TileAcc::TileDType __out__ cMatrix, typename TileLeft::TileDType __in__ aMatrix,
-        typename TileRight::TileDType __in__ bMatrix, uint16_t m, uint16_t k, uint16_t n)
-    {
-        using AType = typename TileLeft::DType;
-        using BType = typename TileRight::DType;
-        using CType = typename TileAcc::DType;
+template <typename TileAcc, typename TileLeft, typename TileRight, bool cmatrixSource, bool CmatrixInitVal>
+__tf__ __aicore__ void TMatmul(typename TileAcc::TileDType __out__ cMatrix, typename TileLeft::TileDType __in__ aMatrix,
+    typename TileRight::TileDType __in__ bMatrix, uint16_t m, uint16_t k, uint16_t n) {
+    using AType = typename TileLeft::DType;
+    using BType = typename TileRight::DType;
+    using CType = typename TileAcc::DType;
 
-        __cc__ CType *c = (__cc__ CType *)(cMatrix);
-        __ca__ AType *a = (__ca__ AType *)(aMatrix);
-        __cb__ BType *b = (__cb__ BType *)(bMatrix);
+    __cc__ CType *c = (__cc__ CType *)(cMatrix);
+    __ca__ AType *a = (__ca__ AType *)(aMatrix);
+    __cb__ BType *b = (__cb__ BType *)(bMatrix);
 
-        bool kDirectionAlign = false;  // only fot f322f32
-        if constexpr ((std::is_same<AType, float>::value) && (std::is_same<CType, float>::value)) {
-            if constexpr(TileLeft::isRowMajor && TileLeft::SFractal == SLayout::ColMajor) {  // zn A transpose
-                kDirectionAlign = true;
-            }
+    bool kDirectionAlign = false; // only for f322f32
+    if constexpr ((std::is_same<AType, float>::value) && (std::is_same<CType, float>::value)) {
+        if constexpr (TileLeft::isRowMajor && TileLeft::SFractal == SLayout::ColMajor) {
+            kDirectionAlign = true;
         }
-
-        // Indicates the Cmatrix source, 1: the C matrix is in bias table buffer, 0: the C matrix is in L0C
-        uint8_t unitFlag = 0;  // unit flag control bits
-
-        mad(c, a, b, m, k, n, unitFlag, kDirectionAlign, cmatrixSource, CmatrixInitVal);
     }
 
-    template <typename TileAcc, typename TileLeft, typename TileRight>
-    __aicore__ PTO_INLINE void Checkstatic() 
-    {
-        static_assert(((std::is_same<typename TileAcc::DType, int32_t>::value) && (std::is_same<typename TileLeft::DType, int8_t>::value) && (std::is_same<typename TileRight::DType, int8_t>::value)) ||
-                      ((std::is_same<typename TileAcc::DType, float>::value) && (std::is_same<typename TileLeft::DType, half>::value) && (std::is_same<typename TileRight::DType, half>::value)) ||
-                      ((std::is_same<typename TileAcc::DType, float>::value) && (std::is_same<typename TileLeft::DType, float>::value) && (std::is_same<typename TileRight::DType, float>::value)) ||
-                      ((std::is_same<typename TileAcc::DType, float>::value) && (std::is_same<typename TileLeft::DType, bfloat16_t>::value) && (std::is_same<typename TileRight::DType, bfloat16_t>::value)),
-                      "The data type is not supported.");
-        constexpr uint16_t m = TileAcc::Rows;
-        constexpr uint16_t n = TileAcc::Cols;
-        constexpr uint16_t k = TileLeft::Cols;
-        constexpr size_t leftSize = (m * k) * sizeof(typename TileLeft::DType);
-        constexpr size_t rightSize = (k * n) * sizeof(typename TileRight::DType);
-        constexpr size_t accSize =(m * n) * sizeof(typename TileAcc::DType);
-        static_assert(leftSize <= 64 * 1024, "The size of left matrix is out of range.");
-        static_assert(rightSize <= 64 * 1024, "The size of right matrix is out of range.");
-        static_assert(accSize <= 256 * 1024, "The size of acc matrix is out of range.");
+    uint8_t unitFlag = 0; // unit flag control bits
 
-        static_assert(TileLeft::Loc == Location::Left, "TileLeft location must be Left!");
-        static_assert(TileRight::Loc == Location::Right, "TileRight location must be Right!");
-        static_assert(TileAcc::Loc == Location::Acc, "TileAcc location must be Acc!");
-    }
-
-    template <typename TileAcc, typename TileLeft, typename TileRight>
-    __aicore__ PTO_INLINE void TMATMUL_IMPL(TileAcc &cMatrix, TileLeft &aMatrix, TileRight &bMatrix)
-    {
-        Checkstatic<TileAcc, TileLeft, TileRight>();
-        // CmatrixInitVal Indicates the initial matrix, 1: the number in C matrix is 0, 0: use the real number in C matrix
-        uint16_t m = aMatrix.GetValidRow();
-        uint16_t k = aMatrix.GetValidCol();
-        uint16_t n = bMatrix.GetValidCol();
-        TMatmul<TileAcc, TileLeft, TileRight, false, true>(cMatrix.data(), aMatrix.data(), bMatrix.data(), m, k, n);
-    }
-
-    template <typename TileAcc, typename TileLeft, typename TileRight>
-    __aicore__ PTO_INLINE void TMATMUL_ACC_IMPL(TileAcc &cOutMatrix, TileAcc &cInMatrix, TileLeft &aMatrix, TileRight &bMatrix)
-    {
-        Checkstatic<TileAcc, TileLeft, TileRight>();
-        // CmatrixInitVal Indicates the initial matrix, 1: the number in C matrix is 0, 0: use the real number in C matrix
-        uint16_t m = aMatrix.GetValidRow();
-        uint16_t k = aMatrix.GetValidCol();
-        uint16_t n = bMatrix.GetValidCol();
-        TMatmul<TileAcc, TileLeft, TileRight, false, false>(cOutMatrix.data(), aMatrix.data(), bMatrix.data(), m, k, n);
-    }
-
-    template <typename TileAcc, typename TileLeft, typename TileRight, typename TileBias>
-    __aicore__ PTO_INLINE void TMATMUL_BIAS_IMPL(TileAcc &cMatrix, TileLeft &aMatrix, TileRight &bMatrix, TileBias &biasMatrix)
-    {
-        Checkstatic<TileAcc, TileLeft, TileRight>();
-        using CType = typename TileAcc::DType;
-        using BiasType = typename TileBias::DType;
-        constexpr bool isBiasValid = std::is_same_v<CType, BiasType>;
-        static_assert(isBiasValid, "No supported bias data type");
-        static_assert(TileBias::Rows == 1, "TileBias must be single row.");
-        static_assert((TileBias::Rows) * sizeof(typename TileAcc::DType) <= 1024, "The size of bias matrix is out of range.");
-        uint16_t m = aMatrix.GetValidRow();
-        uint16_t k = aMatrix.GetValidCol();
-        uint16_t n = bMatrix.GetValidCol();
-
-        __cc__ CType *c = (__cc__ CType *)(cMatrix.data());
-        uint64_t bias = biasMatrix.data();
-        uint64_t xd = ((uint64_t)c) & 0xffffffffULL | ((bias & 0xffffffffULL) << 32);
-
-        TMatmul<TileAcc, TileLeft, TileRight, true, false>((__cc__ CType *)xd, aMatrix.data(), bMatrix.data(), m, k, n);
-    }
+    mad(c, a, b, m, k, n, unitFlag, kDirectionAlign, cmatrixSource, CmatrixInitVal);
 }
+
+template <typename TileAcc, typename TileLeft, typename TileRight>
+__aicore__ PTO_INLINE void CheckStaticMad() {
+    using AType = typename TileLeft::DType;
+    using BType = typename TileRight::DType;
+    using CType = typename TileAcc::DType;
+    static_assert(((std::is_same<CType, int32_t>::value) && (std::is_same<AType, int8_t>::value) &&
+                      (std::is_same<BType, int8_t>::value)) ||
+                      ((std::is_same<CType, float>::value) && (std::is_same<AType, half>::value) &&
+                          (std::is_same<BType, half>::value)) ||
+                      ((std::is_same<CType, float>::value) && (std::is_same<AType, float>::value) &&
+                          (std::is_same<BType, float>::value)) ||
+                      ((std::is_same<CType, float>::value) && (std::is_same<AType, bfloat16_t>::value) &&
+                          (std::is_same<BType, bfloat16_t>::value)),
+        "The data type is not supported.");
+
+    static_assert(
+        (TileLeft::Rows == TileAcc::Rows) && (TileLeft::Cols == TileRight::Rows) && (TileRight::Cols == TileAcc::Cols),
+        "Inconsistent number of m, k, n");
+
+    static_assert(TileLeft::Loc == Location::Left, "TileLeft location must be set to Location::Left.");
+    static_assert(TileRight::Loc == Location::Right, "TileRight location must be set to Location::Right.");
+    static_assert(TileAcc::Loc == Location::Acc, "TileAcc location must be set to Location::Acc.");
+}
+
+__aicore__ PTO_INLINE void CheckDynamicMad(uint16_t aMatrixRow, uint16_t aMatrixCol, uint16_t bMatrixCol) {
+    constexpr uint16_t elementSize = 4095;
+    PTO_ASSERT(aMatrixRow >= 1 && aMatrixRow <= elementSize, "ERROR: The range of valid aMatrixRow is [1, 4095].");
+    PTO_ASSERT(aMatrixCol >= 1 && aMatrixCol <= elementSize, "ERROR: The range of valid aMatrixCol is [1, 4095].");
+    PTO_ASSERT(bMatrixCol >= 1 && bMatrixCol <= elementSize, "ERROR: The range of valid bMatrixCol is [1, 4095].");
+}
+
+template <typename TileAcc, typename TileLeft, typename TileRight>
+__aicore__ PTO_INLINE void TMATMUL_IMPL(TileAcc &cMatrix, TileLeft &aMatrix, TileRight &bMatrix) {
+    CheckStaticMad<TileAcc, TileLeft, TileRight>();
+    uint16_t m = aMatrix.GetValidRow();
+    uint16_t k = aMatrix.GetValidCol();
+    uint16_t n = bMatrix.GetValidCol();
+    CheckDynamicMad(m, k, n);
+    TMatmul<TileAcc, TileLeft, TileRight, false, true>(cMatrix.data(), aMatrix.data(), bMatrix.data(), m, k, n);
+}
+
+template <typename TileAcc, typename TileLeft, typename TileRight>
+__aicore__ PTO_INLINE void TMATMUL_ACC_IMPL(
+    TileAcc &cOutMatrix, TileAcc &cInMatrix, TileLeft &aMatrix, TileRight &bMatrix) {
+    CheckStaticMad<TileAcc, TileLeft, TileRight>();
+    uint16_t m = aMatrix.GetValidRow();
+    uint16_t k = aMatrix.GetValidCol();
+    uint16_t n = bMatrix.GetValidCol();
+    CheckDynamicMad(m, k, n);
+    TMatmul<TileAcc, TileLeft, TileRight, false, false>(cOutMatrix.data(), aMatrix.data(), bMatrix.data(), m, k, n);
+}
+
+template <typename TileAcc, typename TileLeft, typename TileRight, typename TileBias>
+__aicore__ PTO_INLINE void TMATMUL_BIAS_IMPL(
+    TileAcc &cMatrix, TileLeft &aMatrix, TileRight &bMatrix, TileBias &biasData) {
+    CheckStaticMad<TileAcc, TileLeft, TileRight>();
+    using CType = typename TileAcc::DType;
+    using BiasType = typename TileBias::DType;
+    static_assert(std::is_same_v<CType, BiasType>, "No supported bias data type");
+    static_assert((TileBias::Loc == Location::Bias) && (TileBias::Rows == 1), "TileBias must be single row.");
+    uint16_t m = aMatrix.GetValidRow();
+    uint16_t k = aMatrix.GetValidCol();
+    uint16_t n = bMatrix.GetValidCol();
+    CheckDynamicMad(m, k, n);
+
+    __cc__ CType *c = (__cc__ CType *)(cMatrix.data());
+    uint64_t bias = biasData.data();
+    uint64_t xd = ((uint64_t)c) & 0xffffffffULL | ((bias & 0xffffffffULL) << 32);
+
+    TMatmul<TileAcc, TileLeft, TileRight, true, false>((__cc__ CType *)xd, aMatrix.data(), bMatrix.data(), m, k, n);
+}
+} // namespace pto
 #endif
--- test/davinci/v220/src/st/testcase/tmatmul/main.cpp
+++ test/davinci/v220/src/st/testcase/tmatmul/main.cpp
@@ -6,25 +6,21 @@ using namespace std;
 using namespace PtoTestCommon;
 
 template <int32_t tilingKey>
-void launchTMATMUL(uint8_t *out, uint8_t *src0, uint8_t *src1, void *stream);
+void LaunchTMATMUL(uint8_t *out, uint8_t *src0, uint8_t *src1, void *stream);
 
 template <int32_t tilingKey>
-void launchTMATMUL_BIAS(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
+void LaunchTMATMULBIAS(uint8_t *out, uint8_t *src0, uint8_t *src1, uint8_t *src2, void *stream);
 
 class TMATMULTest : public testing::Test {
 protected:
-    void SetUp() override
-    {}
-    void TearDown() override
-    {}
+    void SetUp() override {}
+    void TearDown() override {}
 };
 
 class TMATMULBIASTest : public testing::Test {
 protected:
-    void SetUp() override
-    {}
-    void TearDown() override
-    {}
+    void SetUp() override {}
+    void TearDown() override {}
 };
 
 std::string GetGoldenDir() {
@@ -35,14 +31,10 @@ std::string GetGoldenDir() {
     return fullPath;
 }
 
-TEST_F(TMATMULTest, case1)
-{
-    uint32_t M = 128;
-    uint32_t N = 64;
-    uint32_t K = 128;
-
-    size_t aFileSize = M * K * sizeof(uint16_t);  // uint16_t represent half
-    size_t bFileSize = K * N * sizeof(uint16_t);  // uint16_t represent half
+template <typename T, typename U, typename S, int32_t key>
+void TmatmulTest(uint32_t M, uint32_t K, uint32_t N) {
+    size_t aFileSize = M * K * sizeof(uint16_t); // uint16_t represent half
+    size_t bFileSize = K * N * sizeof(uint16_t); // uint16_t represent half
     size_t cFileSize = M * N * sizeof(float);
 
     aclInit(nullptr);
@@ -66,7 +58,7 @@ TEST_F(TMATMULTest, case1)
 
     aclrtMemcpy(src0Device, aFileSize, src0Host, aFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
     aclrtMemcpy(src1Device, bFileSize, src1Host, bFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
-    launchTMATMUL<1>(dstDevice, src0Device, src1Device, stream);
+    LaunchTMATMUL<key>(dstDevice, src0Device, src1Device, stream);
 
     aclrtSynchronizeStream(stream);
     aclrtMemcpy(dstHost, cFileSize, dstDevice, cFileSize, ACL_MEMCPY_DEVICE_TO_HOST);
@@ -94,127 +86,34 @@ TEST_F(TMATMULTest, case1)
     EXPECT_TRUE(ret);
 }
 
-TEST_F(TMATMULTest, case2)
-{
+TEST_F(TMATMULTest, case1) {
     uint32_t M = 128;
     uint32_t N = 64;
     uint32_t K = 128;
 
-    size_t aFileSize = M * K * sizeof(int8_t);
-    size_t bFileSize = K * N * sizeof(int8_t);
-    size_t cFileSize = M * N * sizeof(int32_t);
-
-    uint8_t *dstHost, *src0Host, *src1Host;
-    uint8_t *dstDevice, *src0Device, *src1Device;
-    aclInit(nullptr);
-    aclrtSetDevice(0);
-    aclrtStream stream;
-    aclrtCreateStream(&stream);
-
-    aclrtMallocHost((void **)(&dstHost), cFileSize);
-    aclrtMallocHost((void **)(&src0Host), aFileSize);
-    aclrtMallocHost((void **)(&src1Host), bFileSize);
-
-    aclrtMalloc((void **)&dstDevice, cFileSize, ACL_MEM_MALLOC_HUGE_FIRST);
-    aclrtMalloc((void **)&src0Device, aFileSize, ACL_MEM_MALLOC_HUGE_FIRST);
-    aclrtMalloc((void **)&src1Device, bFileSize, ACL_MEM_MALLOC_HUGE_FIRST);
-
-    ReadFile(GetGoldenDir() + "/x1_gm.bin", aFileSize, src0Host, aFileSize);
-    ReadFile(GetGoldenDir() + "/x2_gm.bin", bFileSize, src1Host, bFileSize);
-
-    aclrtMemcpy(src0Device, aFileSize, src0Host, aFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
-    aclrtMemcpy(src1Device, bFileSize, src1Host, bFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
-    launchTMATMUL<2>(dstDevice, src0Device, src1Device, stream);
-
-    aclrtSynchronizeStream(stream);
-    aclrtMemcpy(dstHost, cFileSize, dstDevice, cFileSize, ACL_MEMCPY_DEVICE_TO_HOST);
-
-    WriteFile(GetGoldenDir() + "/output_z.bin", dstHost, cFileSize);
-
-    aclrtFree(dstDevice);
-    aclrtFree(src0Device);
-    aclrtFree(src1Device);
-
-    aclrtFreeHost(dstHost);
-    aclrtFreeHost(src0Host);
-    aclrtFreeHost(src1Host);
-    aclrtDestroyStream(stream);
-    aclrtResetDevice(0);
-    aclFinalize();
-
-    std::vector<int32_t> golden(cFileSize);
-    std::vector<int32_t> devFinal(cFileSize);
-    ReadFile(GetGoldenDir() + "/golden.bin", cFileSize, golden.data(), cFileSize);
-    ReadFile(GetGoldenDir() + "/output_z.bin", cFileSize, devFinal.data(), cFileSize);
-
-    bool ret = ResultCmp<int32_t>(golden, devFinal, 0.001f);
-    EXPECT_TRUE(ret);
+    TmatmulTest<float, uint16_t, uint16_t, 1>(M, K, N);
 }
 
-TEST_F(TMATMULTest, case3)
-{
+TEST_F(TMATMULTest, case2) {
+    uint32_t M = 128;
+    uint32_t N = 64;
+    uint32_t K = 128;
+
+    TmatmulTest<int32_t, int8_t, int8_t, 2>(M, K, N);
+}
+
+TEST_F(TMATMULTest, case3) {
     uint32_t M = 127;
     uint32_t N = 63;
-    uint32_t K = 128;
+    uint32_t K = 63;
 
-    size_t aFileSize = M * K * sizeof(uint16_t);  // uint16_t represent half
-    size_t bFileSize = K * N * sizeof(uint16_t);  // uint16_t represent half
-    size_t cFileSize = M * N * sizeof(float);
-
-    aclInit(nullptr);
-    aclrtSetDevice(0);
-    aclrtStream stream;
-    aclrtCreateStream(&stream);
-
-    uint8_t *dstHost, *src0Host, *src1Host;
-    uint8_t *dstDevice, *src0Device, *src1Device;
-
-    aclrtMallocHost((void **)(&dstHost), cFileSize);
-    aclrtMallocHost((void **)(&src0Host), aFileSize);
-    aclrtMallocHost((void **)(&src1Host), bFileSize);
-
-    aclrtMalloc((void **)&dstDevice, cFileSize, ACL_MEM_MALLOC_HUGE_FIRST);
-    aclrtMalloc((void **)&src0Device, aFileSize, ACL_MEM_MALLOC_HUGE_FIRST);
-    aclrtMalloc((void **)&src1Device, bFileSize, ACL_MEM_MALLOC_HUGE_FIRST);
-
-    ReadFile(GetGoldenDir() + "/x1_gm.bin", aFileSize, src0Host, aFileSize);
-    ReadFile(GetGoldenDir() + "/x2_gm.bin", bFileSize, src1Host, bFileSize);
-
-    aclrtMemcpy(src0Device, aFileSize, src0Host, aFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
-    aclrtMemcpy(src1Device, bFileSize, src1Host, bFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
-    launchTMATMUL<3>(dstDevice, src0Device, src1Device, stream);
-
-    aclrtSynchronizeStream(stream);
-    aclrtMemcpy(dstHost, cFileSize, dstDevice, cFileSize, ACL_MEMCPY_DEVICE_TO_HOST);
-
-    WriteFile(GetGoldenDir() + "/output_z.bin", dstHost, cFileSize);
-
-    aclrtFree(dstDevice);
-    aclrtFree(src0Device);
-    aclrtFree(src1Device);
-
-    aclrtFreeHost(dstHost);
-    aclrtFreeHost(src0Host);
-    aclrtFreeHost(src1Host);
-    aclrtDestroyStream(stream);
-    aclrtResetDevice(0);
-    aclFinalize();
-
-    std::vector<float> golden(cFileSize);
-    std::vector<float> devFinal(cFileSize);
-
-    ReadFile(GetGoldenDir() + "/golden.bin", cFileSize, golden.data(), cFileSize);
-    ReadFile(GetGoldenDir() + "/output_z.bin", cFileSize, devFinal.data(), cFileSize);
-
-    bool ret = ResultCmp(golden, devFinal, 0.001f);
-    EXPECT_TRUE(ret);
+    TmatmulTest<float, uint16_t, uint16_t, 3>(M, K, N);
 }
 
 template <typename T, typename U, typename S, typename biasType, int32_t key>
-void tmatmul_bias_test(uint32_t M, uint32_t K, uint32_t N){
-
-    size_t aFileSize = M * K * sizeof(U);  // uint16_t represent half
-    size_t bFileSize = K * N * sizeof(S);  // uint16_t represent half
+void TmatmulBiasTest(uint32_t M, uint32_t K, uint32_t N) {
+    size_t aFileSize = M * K * sizeof(U); // uint16_t represent half
+    size_t bFileSize = K * N * sizeof(S); // uint16_t represent half
     size_t cFileSize = M * N * sizeof(T);
     size_t biasFileSize = 1 * N * sizeof(biasType);
 
@@ -245,7 +144,7 @@ void tmatmul_bias_test(uint32_t M, uint32_t K, uint32_t N){
     aclrtMemcpy(src0Device, aFileSize, src0Host, aFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
     aclrtMemcpy(src1Device, bFileSize, src1Host, bFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
     aclrtMemcpy(src2Device, biasFileSize, src2Host, biasFileSize, ACL_MEMCPY_HOST_TO_DEVICE);
-    launchTMATMUL_BIAS<key>(dstDevice, src0Device, src1Device, src2Device, stream);
+    LaunchTMATMULBIAS<key>(dstDevice, src0Device, src1Device, src2Device, stream);
 
     aclrtSynchronizeStream(stream);
     aclrtMemcpy(dstHost, cFileSize, dstDevice, cFileSize, ACL_MEMCPY_DEVICE_TO_HOST);
@@ -275,65 +174,58 @@ void tmatmul_bias_test(uint32_t M, uint32_t K, uint32_t N){
     EXPECT_TRUE(ret);
 }
 
-TEST_F(TMATMULBIASTest, case1){
-
+TEST_F(TMATMULBIASTest, case1) {
     uint32_t M = 128;
     uint32_t K = 128;
     uint32_t N = 64;
 
-    tmatmul_bias_test<float, uint16_t, uint16_t, float, 1>(M, K, N);
+    TmatmulBiasTest<float, uint16_t, uint16_t, float, 1>(M, K, N);
 }
 
-TEST_F(TMATMULBIASTest, case2){
-
+TEST_F(TMATMULBIASTest, case2) {
     uint32_t M = 128;
     uint32_t K = 128;
     uint32_t N = 63;
 
-    tmatmul_bias_test<float, uint16_t, uint16_t, float, 2>(M, K, N);
+    TmatmulBiasTest<float, uint16_t, uint16_t, float, 2>(M, K, N);
 }
 
-TEST_F(TMATMULBIASTest, case3){
-
+TEST_F(TMATMULBIASTest, case3) {
     uint32_t M = 15;
     uint32_t K = 16;
     uint32_t N = 15;
 
-    tmatmul_bias_test<float, float, float, float, 3>(M, K, N);
+    TmatmulBiasTest<float, float, float, float, 3>(M, K, N);
 }
 
-TEST_F(TMATMULBIASTest, case4){
-
+TEST_F(TMATMULBIASTest, case4) {
     uint32_t M = 127;
     uint32_t K = 127;
     uint32_t N = 63;
 
-    tmatmul_bias_test<int32_t, int8_t, int8_t, int32_t, 4>(M, K, N);
+    TmatmulBiasTest<int32_t, int8_t, int8_t, int32_t, 4>(M, K, N);
 }
 
-TEST_F(TMATMULBIASTest, case5){
-
+TEST_F(TMATMULBIASTest, case5) {
     uint32_t M = 128;
     uint32_t K = 128;
     uint32_t N = 64;
 
-    tmatmul_bias_test<float, uint16_t, uint16_t, float, 5>(M, K, N);
+    TmatmulBiasTest<float, uint16_t, uint16_t, float, 5>(M, K, N);
 }
 
-TEST_F(TMATMULBIASTest, case6){
-
+TEST_F(TMATMULBIASTest, case6) {
     uint32_t M = 128;
     uint32_t K = 128;
     uint32_t N = 64;
 
-    tmatmul_bias_test<int32_t, int8_t, int8_t, int32_t, 6>(M, K, N);
+    TmatmulBiasTest<int32_t, int8_t, int8_t, int32_t, 6>(M, K, N);
 }
 
-TEST_F(TMATMULBIASTest, case7){
-
+TEST_F(TMATMULBIASTest, case7) {
     uint32_t M = 128;
     uint32_t K = 128;
     uint32_t N = 64;
 
-    tmatmul_bias_test<int32_t, int8_t, int8_t, int32_t, 7>(M, K, N);
+    TmatmulBiasTest<int32_t, int8_t, int8_t, int32_t, 7>(M, K, N);
 }
