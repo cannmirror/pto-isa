e5a17a11b1362971d1a198b15589085f94b27925
turing_project1 <songchangxia@cann.team>
2025-12-05 22:26:43 +0800

    A5 binary scalar template + TMins
    Co-authored-by: ichernob<chernobaev.igor@huawei.com>
    Co-authored-by: danilsaut<sautenkov.danil@h-partners.com>

--- include/common/pto_instr_impl.hpp
+++ include/common/pto_instr_impl.hpp
@@ -88,6 +88,7 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #include "davinci/v310/TGather.hpp"
 #include "davinci/v310/TUnaryOp.hpp"
 #include "davinci/v310/TGatherB.hpp"
+#include "davinci/v310/TBinSOp.hpp"
 #endif
 
 #ifdef __CPU_SIM
--- include/davinci/v310/TMins.hpp
+++ include/davinci/v310/TMins.hpp
@@ -15,74 +15,36 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #include "common/utils.hpp"
 #include "common.hpp"
 #include "utils.hpp"
+#include "TBinSOp.hpp"
 
 namespace pto {
-template <typename TileData, typename ScalarType, unsigned elementsPerRepeat, unsigned blockSizeElem>
-__tf__ __aicore__ PTO_INLINE void TMinsImpl(
-    typename TileData::TileDType __out__ dst,
-    typename TileData::TileDType __in__ src0,
-    ScalarType scalar,
-    unsigned validRow
-) {
+
+template <typename T> struct MinSOp {
+    __aicore__ PTO_INLINE static void BinSInstr(RegTensor<T> &reg_dst, RegTensor<T> &reg_src0, T src1, MaskReg &preg)
+    {
+        vmins(reg_dst, reg_src0, src1, preg, MODE_ZEROING);
+    }
+};
+
+template <typename TileData, unsigned elementsPerRepeat, unsigned blockSizeElem, unsigned rowStride>
+__tf__ __aicore__ PTO_INLINE
+void TMinS(typename TileData::TileDType __out__ dst, 
+           typename TileData::TileDType __in__ src0, 
+           typename TileData::DType src1,
+           unsigned kValidRows,
+           unsigned kValidCols,
+           BinSOpsImpl version = BinSOpsImpl::BinSOpsIMPL_DEFAULT) {
     using T = typename TileData::DType;
     __ubuf__ T *dstPtr = (__ubuf__ T *)__cce_get_tile_ptr(dst);
     __ubuf__ T *src0Ptr = (__ubuf__ T *)__cce_get_tile_ptr(src0);
-
-    __VEC_SCOPE__
-    {
-        RegTensor<T> vreg0;
-        RegTensor<T> vreg2;
-        MaskReg preg;
-        uint32_t sreg = (uint32_t)(validRow * TileData::Cols);
-        uint16_t repeatTimes = CeilDivision(validRow * TileData::Cols, elementsPerRepeat);
-        constexpr auto distValue =
-            std::integral_constant<::DistVST, static_cast<::DistVST>(GetDistVst<T, DistVST::DIST_NORM>())>();
-        for (uint16_t i = 0; i < repeatTimes; ++i) {
-            preg = CreatePredicate<T>(sreg);
-            vlds(vreg0, src0Ptr, elementsPerRepeat, NORM, POST_UPDATE);
-            vmins(vreg2, vreg0, scalar, preg, MODE_ZEROING);
-            vsts(vreg2, dstPtr, elementsPerRepeat, distValue, preg, POST_UPDATE);
-        }
-    } // end of VF
-}
-
-template <typename TileData, typename ScalarType, unsigned elementsPerRepeat, unsigned blockSizeElem>
-__tf__ __aicore__ PTO_INLINE void TMinsPadImpl(
-    typename TileData::TileDType __out__ dst,
-    typename TileData::TileDType __in__ src0,
-    ScalarType scalar,
-    unsigned validRow,
-    unsigned validCol
-) {
-    using T = typename TileData::DType;
-    __ubuf__ T *dstPtr = (__ubuf__ T *)__cce_get_tile_ptr(dst);
-    __ubuf__ T *src0Ptr = (__ubuf__ T *)__cce_get_tile_ptr(src0);
-
-    __VEC_SCOPE__
-    {
-        RegTensor<T> vreg0;
-        RegTensor<T> vreg2;
-        MaskReg preg;
-        uint16_t repeatTimes = CeilDivision(validCol, elementsPerRepeat);
-        constexpr auto distValue =
-            std::integral_constant<::DistVST, static_cast<::DistVST>(GetDistVst<T, DistVST::DIST_NORM>())>();
-        for (uint16_t i = 0; i < (uint16_t)(validRow); ++i) {
-            uint32_t sreg = (uint32_t)(validCol);
-            for (uint16_t j = 0; j < (uint16_t)repeatTimes; ++j) {
-                preg = CreatePredicate<T>(sreg);
-                vlds(vreg0, src0Ptr + i * TileData::Cols, j * elementsPerRepeat, NORM);
-                vmins(vreg2, vreg0, scalar, preg, MODE_ZEROING);
-                vsts(vreg2, dstPtr + i * TileData::Cols, j * elementsPerRepeat, distValue, preg);
-            }
-        }
-    } // end VF
+    BinaryInstr<MinSOp<T>, TileData, T, elementsPerRepeat, blockSizeElem, rowStride>(
+                dstPtr, src0Ptr, src1, kValidRows, kValidCols, version);
 }
 
 template <typename TileData>
-__aicore__ void TMINS_IMPL(TileData &dst, TileData &src0, typename TileData::DType scalar)
+__aicore__ void TMINS_IMPL(TileData &dst, TileData &src0, typename TileData::DType src1)
 {
     using T = typename TileData::DType;
-    static_assert(sizeof(T) == 4 || sizeof(T) == 2 || sizeof(T) == 1, "TMINS: Invalid data type.");
     static_assert(TileData::Loc == Location::Vec, "Location of src and dst tiles must be Location::Vec.");
     static_assert(TileData::ValidCol <= TileData::Cols, "Number of valid columns must not be greater than number of tile columns.");
     static_assert(TileData::ValidRow <= TileData::Rows, "Number of valid rows must not be greater than number of tile rows.");
@@ -95,11 +57,7 @@ __aicore__ void TMINS_IMPL(TileData &dst, TileData &src0, typename TileData::DTy
 
     PTO_ASSERT(src0.GetValidCol() == dst.GetValidCol(), "Number of columns of src and dst must be the same.");
 
-    if constexpr (TileData::PadVal == PadValue::Null || TileData::PadVal == PadValue::Zero) {
-        TMinsImpl<TileData, T, elementsPerRepeat, blockSizeElem>(dst.data(), src0.data(), scalar, validRow);
-    } else { // -INF(MIN) or INF(MAX)
-        TMinsPadImpl<TileData, T, elementsPerRepeat, blockSizeElem>(dst.data(), src0.data(), scalar, validRow, validCol);
-    }
+    TMinS<TileData, elementsPerRepeat, blockSizeElem, rowStride>(dst.data(), src0.data(), src1, validRow, validCol);
 }
 }  // namespace pto
 #endif
