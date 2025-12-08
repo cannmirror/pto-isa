0da218049bb4268646fbe0edd98a1023ba5e08df
Liang_vip10 <yangweiliang3@huawei.com>
2025-12-08 19:02:27 +0800

    TColReduce Code
    Co-authored-by: yangweiliang<yangweiliang3@huawei.com>

--- include/pto/common/pto_instr_impl.hpp
+++ include/pto/common/pto_instr_impl.hpp
@@ -54,6 +54,7 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #include "pto/npu/a2a3/TColSum.hpp"
 #include "pto/npu/a2a3/TUnaryOp.hpp"
 #include "pto/npu/a2a3/TGatherB.hpp"
+#include "pto/npu/a2a3/TColMin.hpp"
 #endif
 
 #ifdef __DAV_V310
