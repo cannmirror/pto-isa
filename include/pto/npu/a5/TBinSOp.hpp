3635e8af3e8b728ce4d1aed29ee0e9736e5851a0
danilsaut <sautenkov.danil@h-partners.com>
2025-12-09 23:16:59 +0800

    Codecheck fixes for ScalarTemplate A5
    Co-authored-by: danilsaut<sautenkov.danil@h-partners.com>

--- include/pto/npu/a5/TBinSOp.hpp
+++ include/pto/npu/a5/TBinSOp.hpp
@@ -64,21 +64,21 @@ void TBinSOps_1D_PostUpdate(__ubuf__ typename TileData::DType *dstPtr,
                     unsigned kValidRows,
                     unsigned kValidCols) {
     using T = typename TileData::DType;
-    uint16_t repeatTimes = CeilDivision(kValidRows * kValidCols, elementsPerRepeat);
+    uint16_t repeatTimes_pu = CeilDivision(kValidRows * kValidCols, elementsPerRepeat);
     __VEC_SCOPE__
     {
-        RegTensor<T> vreg0;
-        RegTensor<T> vreg2;
-        MaskReg  preg;
+        RegTensor<T> vreg0_pu;
+        RegTensor<T> vreg2_pu;
+        MaskReg  preg_pu;
 
-        constexpr auto distValue = std::integral_constant<::DistVST, static_cast<::DistVST>(GetDistVst<T, DistVST::DIST_NORM>())>();
-        unsigned sreg = kValidRows * kValidCols;
+        constexpr auto distValue_pu = std::integral_constant<::DistVST, static_cast<::DistVST>(GetDistVst<T, DistVST::DIST_NORM>())>();
+        unsigned sreg_pu = kValidRows * kValidCols;
         #pragma clang loop unroll(disable)
-        for (uint16_t i = 0; i < (uint16_t)repeatTimes; ++i) {
-            preg = CreatePredicate<T>(sreg);
-            vlds(vreg0, src0Ptr, elementsPerRepeat, NORM, POST_UPDATE);
-            Op::BinSInstr(vreg2, vreg0, src1, preg);
-            vsts(vreg2, dstPtr, elementsPerRepeat, distValue, preg, POST_UPDATE);
+        for (uint16_t i = 0; i < (uint16_t)repeatTimes_pu; ++i) {
+            preg_pu = CreatePredicate<T>(sreg_pu);
+            vlds(vreg0_pu, src0Ptr, elementsPerRepeat, NORM, POST_UPDATE);
+            Op::BinSInstr(vreg2_pu, vreg0_pu, src1, preg_pu);
+            vsts(vreg2_pu, dstPtr, elementsPerRepeat, distValue_pu, preg_pu, POST_UPDATE);
         }
     } 
 }
@@ -119,26 +119,45 @@ void TBinSOps_2D_PostUpdate(__ubuf__ typename TileData::DType *dstPtr,
                     unsigned kValidRows,
                     unsigned kValidCols) {
     using T = typename TileData::DType;
-    uint16_t repeatTimes = CeilDivision(kValidCols, elementsPerRepeat);
+    uint16_t repeatTimes_pu = CeilDivision(kValidCols, elementsPerRepeat);
 
     __VEC_SCOPE__
     {
-        RegTensor<T> vreg0;
-        RegTensor<T> vreg2;
-        MaskReg preg;
-        constexpr auto distValue = std::integral_constant<::DistVST, static_cast<::DistVST>(GetDistVst<T, DistVST::DIST_NORM>())>();
+        RegTensor<T> vreg0_pu;
+        RegTensor<T> vreg2_pu;
+        MaskReg preg_pu;
+        constexpr auto distValue_pu = std::integral_constant<::DistVST, static_cast<::DistVST>(GetDistVst<T, DistVST::DIST_NORM>())>();
         for (uint16_t i = 0; i < (uint16_t)(kValidRows); ++i) {
-            for (uint16_t j = 0; j < (uint16_t)repeatTimes; ++j) {
-                vlds(vreg0, src0Ptr,  i * rowStride + j * elementsPerRepeat, NORM);
-                uint32_t count = ((j + 1) * elementsPerRepeat >= kValidCols ? kValidCols - j * elementsPerRepeat : elementsPerRepeat);
-                preg = CreatePredicate<T>(count);
-                Op::BinSInstr(vreg2, vreg0, src1, preg);
-                vsts(vreg2, dstPtr, i * rowStride + j * elementsPerRepeat, distValue, preg);
+            for (uint16_t j = 0; j < (uint16_t)repeatTimes_pu; ++j) {
+                vlds(vreg0_pu, src0Ptr,  i * rowStride + j * elementsPerRepeat, NORM);
+                uint32_t count_pu = ((j + 1) * elementsPerRepeat >= kValidCols ? kValidCols - j * elementsPerRepeat : elementsPerRepeat);
+                preg_pu = CreatePredicate<T>(count_pu);
+                Op::BinSInstr(vreg2_pu, vreg0_pu, src1, preg_pu);
+                vsts(vreg2_pu, dstPtr, i * rowStride + j * elementsPerRepeat, distValue_pu, preg_pu);
             }
         }
     }
 }
-
+template <typename Op, typename TileData, typename ScalarType, unsigned elementsPerRepeat, unsigned blockSizeElem, unsigned rowStride>
+__aicore__ PTO_INLINE void TBinSOps_1D_selector(typename TileData::TileDType __out__ dst, 
+                            typename TileData::TileDType __in__ src0, 
+                            ScalarType src1,
+                            unsigned kValidRows,
+                            unsigned kValidCols,
+                            BinSOpsImpl version) {
+        switch (version) {
+        case BinSOpsImpl::BinSOpsIMPL_1D_NO_POST_UPDATE:
+        case BinSOpsImpl::BinSOpsIMPL_2D_NO_POST_UPDATE:
+            TBinSOps_1D_NoPostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
+            break;
+        case BinSOpsImpl::BinSOpsIMPL_DEFAULT:
+        case BinSOpsImpl::BinSOpsIMPL_1D_POST_UPDATE:
+        case BinSOpsImpl::BinSOpsIMPL_2D_POST_UPDATE:
+        default:
+            TBinSOps_1D_PostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
+            break;
+        }
+} 
 template <typename Op, typename TileData, typename ScalarType, unsigned elementsPerRepeat, unsigned blockSizeElem, unsigned rowStride>
 PTO_INTERNAL void BinaryInstr(typename TileData::TileDType __out__ dst,
                             typename TileData::TileDType __in__ src0, 
@@ -147,51 +166,23 @@ PTO_INTERNAL void BinaryInstr(typename TileData::TileDType __out__ dst,
                             unsigned kValidCols,
                             BinSOpsImpl version) {
     using T = typename TileData::DType;
-    if constexpr (std::is_same_v<T, uint8_t> || std::is_same_v<T, int8_t> || std::is_same_v<T, uint16_t> || 
+    static_assert(std::is_same_v<T, uint8_t> || std::is_same_v<T, int8_t> || std::is_same_v<T, uint16_t> || 
         std::is_same_v<T, int16_t> || std::is_same_v<T, uint32_t> ||
         std::is_same_v<T, int32_t> || std::is_same_v<T, half> ||
-        std::is_same_v<T, float> || std::is_same_v<T, bfloat16_t>) {
+        std::is_same_v<T, float> || std::is_same_v<T, bfloat16_t>,  "TBinSOps: Invalid data type."); 
         if constexpr (TileData::ValidCol == TileData::Cols) {
-            switch (version) {
-            case BinSOpsImpl::BinSOpsIMPL_DEFAULT:
-                TBinSOps_1D_PostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
-                break;
-            case BinSOpsImpl::BinSOpsIMPL_1D_NO_POST_UPDATE:
-            case BinSOpsImpl::BinSOpsIMPL_2D_NO_POST_UPDATE:
-                TBinSOps_1D_NoPostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
-                break;
-            case BinSOpsImpl::BinSOpsIMPL_1D_POST_UPDATE:
-            case BinSOpsImpl::BinSOpsIMPL_2D_POST_UPDATE:
-            default:
-                TBinSOps_1D_PostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
-                break;
-            }
+            TBinSOps_1D_selector<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols, version);
         } else {
             if (TileData::Cols == kValidCols) {
-                switch (version) {
-                case BinSOpsImpl::BinSOpsIMPL_DEFAULT:
-                    TBinSOps_1D_PostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
-                    break;
-                case BinSOpsImpl::BinSOpsIMPL_1D_NO_POST_UPDATE:
-                case BinSOpsImpl::BinSOpsIMPL_2D_NO_POST_UPDATE:
-                    TBinSOps_1D_NoPostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
-                    break;
-                case BinSOpsImpl::BinSOpsIMPL_1D_POST_UPDATE:
-                case BinSOpsImpl::BinSOpsIMPL_2D_POST_UPDATE:
-                default:
-                    TBinSOps_1D_PostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
-                    break;
-                }  
+                TBinSOps_1D_selector<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols, version);  
             }
             else {
                 switch (version) {
-                case BinSOpsImpl::BinSOpsIMPL_DEFAULT:
-                    TBinSOps_2D_PostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
-                    break;
                 case BinSOpsImpl::BinSOpsIMPL_1D_NO_POST_UPDATE:
                 case BinSOpsImpl::BinSOpsIMPL_2D_NO_POST_UPDATE:
                     TBinSOps_2D_NoPostUpdate<Op, TileData, ScalarType, elementsPerRepeat, blockSizeElem, rowStride>(dst, src0, src1, kValidRows, kValidCols);
                     break;
+                case BinSOpsImpl::BinSOpsIMPL_DEFAULT:
                 case BinSOpsImpl::BinSOpsIMPL_1D_POST_UPDATE:
                 case BinSOpsImpl::BinSOpsIMPL_2D_POST_UPDATE:
                 default:
@@ -200,10 +191,6 @@ PTO_INTERNAL void BinaryInstr(typename TileData::TileDType __out__ dst,
                 }  
             }
         }
-    }else {
-        static_assert(sizeof(T) == 4 || sizeof(T) == 2 || sizeof(T) == 1, "TBinSOps: Invalid data type.");
     }
-}
-
 }  // namespace pto
 #endif
