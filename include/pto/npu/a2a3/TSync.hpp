1e99cf65b7693c0cddcc152a11edf5b605ad5945
HuangXingYuan_777 <huangxingyuan4@huawei.com>
2025-12-17 18:18:24 +0800

    update tmatmul st
    Co-authored-by: HuangXingYuan_777<huangxingyuan4@huawei.com>

--- include/pto/npu/a2a3/TSync.hpp
+++ include/pto/npu/a2a3/TSync.hpp
@@ -13,72 +13,22 @@ See LICENSE in the root of the software repository for the full text of the Lice
 
 #include <pto/common/type.hpp>
 
+#define FFTS_BASE_COUNT_WIDTH 0xf
+#define FFTS_MODE_VAL 0x2
+#define FFTS_MODE_WIDTH 0x3
+#define FFTS_MODE_OFFSET 4
+#define FFTS_EVENT_ID_WIDTH 0xf
+#define FFTS_EVENT_ID_OFFSET 8
 namespace pto {
-  // opPipeList maps each operation in Op enum to its corresponding pipeline type.
-  // This array is used to determine which hardware pipeline should be used for each operation.
-  constexpr pipe_t opPipeList[] = {
-    PIPE_S,     // SCALAR
-#ifdef __DAV_VEC__
-    PIPE_V,     // VECTOR
-    PIPE_V,     // TADD
-    PIPE_V,     // TADDS
-    PIPE_V,     // TSUB
-    PIPE_V,     // TMUL
-    PIPE_V,     // TMULS
-    PIPE_V,     // TDIV
-    PIPE_V,     // TDIVS
-    PIPE_V,     // TMIN
-    PIPE_V,     // TMINS
-    PIPE_V,     // TMAX
-    PIPE_V,     // TSEL
-    PIPE_V,     // TEXP
-    PIPE_V,     // TSELS
-    PIPE_V,     // TSQRT
-    PIPE_V,     // TRSQRT
-    PIPE_V,     // TEXPANDS
-    PIPE_V,     // TPARTADD
-    PIPE_V,     // TPARTMAX
-    PIPE_V,     // TPARTMIN
-    PIPE_V,     // TCMPS
-    PIPE_V,     // TMRGSORT
-    PIPE_V,     // TSORT32
-    PIPE_V,     // TCI
-    PIPE_V,     // TGATHER
-    PIPE_V,     // TGATHERB
-    PIPE_V,     // TCVT
-    PIPE_V,     // TMOV_V2V
-    PIPE_V,     // TROWSUM
-    PIPE_V,     // TROWMAX
-    PIPE_V,     // TROWMIN
-    PIPE_V,     // TROWEXPAND
-    PIPE_V,     // TCOLSUM
-    PIPE_V,     // TCOLMAX
-    PIPE_V,     // TCOLMIN
-    PIPE_V,     // TTRANS
-    PIPE_MTE3,  // TSTORE_VEC
-    PIPE_MTE2,  // TLOAD
-#endif
-#ifdef __DAV_CUBE__
-    PIPE_MTE1,  // TMOV_M2B
-    PIPE_MTE1,  // TMOV_M2L
-    PIPE_MTE1,  // TMOV_M2R
-    PIPE_FIX,   // TMOV_M2S
-    PIPE_FIX,   // TMOV_A2V
-    PIPE_FIX,   // TSTORE_ACC
-    PIPE_M,     // TMATMUL
-    PIPE_M,     // TMATMUL_ACC
-    PIPE_M,     // TMATMUL_BIAS
-    PIPE_MTE1,  // TEXTRACT
-#endif
-  };
-
   PTO_INTERNAL uint16_t getFFTSMsg(uint16_t mode, uint16_t eventId, uint16_t baseConst = 0x1) {
-    return ((baseConst & 0xf) + ((mode & 0x3) << 4) + ((eventId & 0xf) << 8));
+    return ((baseConst & FFTS_BASE_COUNT_WIDTH) +
+      ((mode & FFTS_MODE_WIDTH) << FFTS_MODE_OFFSET) +
+      ((eventId & FFTS_EVENT_ID_WIDTH) << FFTS_EVENT_ID_OFFSET));
   }
 
   PTO_INTERNAL void CrossCoreEventRecord(pipe_t pipe, uint16_t eventId) {
 #ifdef __DAV_CUBE__
-    ffts_cross_core_sync(pipe, getFFTSMsg(0x2, eventId));
+    ffts_cross_core_sync(pipe, getFFTSMsg(FFTS_MODE_VAL, eventId));
 #endif
   }
 
